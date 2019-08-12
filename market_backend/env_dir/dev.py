61ccdba3bd77af76d47e0907c3d7c0833320d381
Muthu Kumar <muthukumar4999@gmail.com>
2019-08-12 13:00:52 +0530

    Update dev.py

--- market_backend/env_dir/dev.py
+++ market_backend/env_dir/dev.py
@@ -30,9 +30,9 @@ DATABASES['default'].update(db_from_env)
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
-AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME', 'mr-sbs')
-AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', 'AKIAIMV5CDFIT32UNOLQ')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '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')
-AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', 'us-east-1')
+AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME', '')
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', '')
 
 AWS_S3_BASE_LINK = "https://s3.amazonaws.com/{}/".format(AWS_BUCKET_NAME)
